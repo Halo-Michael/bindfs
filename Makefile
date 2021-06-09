f22f431c9977e39ffbc88e10202767d8de979fb2
Halo-Michael <yc992147156@me.com>
2021-06-10 02:59:04 +0800

    First commit

---
+++ Makefile
@@ -0,0 +1,24 @@
+TARGET = bindfs
+VERSION = 0.1.0
+CC = xcrun -sdk iphoneos clang -arch arm64 -Os
+LDID = ldid
+SED = gsed
+
+.PHONY: all clean
+
+all: clean mount_bindfs
+	mkdir com.michael.bindfs_$(VERSION)_iphoneos-arm
+	mkdir com.michael.bindfs_$(VERSION)_iphoneos-arm/DEBIAN
+	cp control com.michael.bindfs_$(VERSION)_iphoneos-arm/DEBIAN
+	$(SED) -i 's/^Version:\x24/Version: $(VERSION)/g' com.michael.bindfs_$(VERSION)_iphoneos-arm/DEBIAN/control
+	mkdir com.michael.bindfs_$(VERSION)_iphoneos-arm/sbin
+	mv mount_bindfs com.michael.bindfs_$(VERSION)_iphoneos-arm/sbin
+	dpkg -b com.michael.bindfs_$(VERSION)_iphoneos-arm
+
+mount_bindfs: clean
+	$(CC) mount_bindfs.c -o mount_bindfs -lutil
+	strip mount_bindfs
+	$(LDID) -Sentitlements.xml mount_bindfs
+
+clean:
+	rm -rf com.michael.bindfs_* mount_bindfs
---
+++ mount_bindfs.c
@@ -0,0 +1,65 @@
+#include <err.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mount.h>
+#include <unistd.h>
+#include "mntopts.h"
+
+struct mntopt mopts[] = {
+    MOPT_STDOPTS,
+    MOPT_IGNORE_OWNERSHIP,
+    MOPT_PERMISSIONS,
+    MOPT_UPDATE,
+    { NULL }
+};
+
+void usage() {
+    puts("mount_bindfs [options] dir mountpoint");
+    puts("options:");
+    puts("\t-o opt[,opt,...]\tMount options.");
+}
+
+int main(int argc, char **argv) {
+    int ch, mntflags = 0;
+    while ((ch = getopt(argc, argv, "ho:")) != EOF) {
+        switch (ch) {
+        case 'o':
+            {
+                int dummy;
+                getmntopts(optarg, mopts, &mntflags, &dummy);
+            }
+            break;
+        default:
+            {
+                usage();
+                return 1;
+            }
+        }
+    }
+    argc -= optind;
+    argv += optind;
+
+    if (argc != 2) {
+        usage();
+        return 1;
+    }
+
+    char *dir = (char *)calloc(MAXPATHLEN, sizeof(char));
+    if (realpath(argv[1], dir) == NULL)
+        err(errno, "realpath dir %s", dir);
+    dir = (char *)realloc(dir, (strlen(dir) + 1) * sizeof(char));
+
+    char *mountpoint = (char *)calloc(MAXPATHLEN, sizeof(char));
+    if (realpath(argv[0], mountpoint) == NULL)
+        err(errno, "realpath mountpoint %s", mountpoint);
+    mountpoint = (char *)realloc(mountpoint, (strlen(mountpoint) + 1) * sizeof(char));
+
+    int mountStatus;
+    if ((mountStatus = mount("bindfs", dir, mntflags, mountpoint)) < 0) {
+        err(errno, "error on mount(): error = %d", mountStatus);
+    };
+
+    return 0;
+}
