f46e2e36a5e95e23e09f86eab7784ed22f7e87b6
Halo-Michael <yc992147156@me.com>
2021-06-10 03:27:01 +0800

    Update

--- mount_bindfs.c
+++ mount_bindfs.c
@@ -45,18 +45,18 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    char *mountpoint = (char *)calloc(MAXPATHLEN, sizeof(char));
-    if (realpath(argv[0], mountpoint) == NULL)
-        err(errno, "realpath mountpoint %s", mountpoint);
-    mountpoint = (char *)realloc(mountpoint, (strlen(mountpoint) + 1) * sizeof(char));
-
     char *dir = (char *)calloc(MAXPATHLEN, sizeof(char));
     if (realpath(argv[1], dir) == NULL)
         err(errno, "realpath dir %s", dir);
     dir = (char *)realloc(dir, (strlen(dir) + 1) * sizeof(char));
 
+    char *mountpoint = (char *)calloc(MAXPATHLEN, sizeof(char));
+    if (realpath(argv[0], mountpoint) == NULL)
+        err(errno, "realpath mountpoint %s", mountpoint);
+    mountpoint = (char *)realloc(mountpoint, (strlen(mountpoint) + 1) * sizeof(char));
+
     int mountStatus;
-    if ((mountStatus = mount("bindfs", dir, mntflags, mountpoint)) < 0) {
+    if ((mountStatus = mount("bindfs", mountpoint, mntflags, dir)) < 0) {
         err(errno, "error on mount(): error = %d", mountStatus);
     };
 
